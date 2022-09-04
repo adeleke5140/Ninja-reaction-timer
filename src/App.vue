b5fbdf8b76b6b4bb90bc8404462696c62e36a085
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-09-04 20:53:06 +0100

    add inital files for ninja reaction timer

---
+++ src/App.vue
@@ -0,0 +1,38 @@
+<script setup>
+import { ref } from "vue"
+import Cursor1 from "./components/cursor1.vue"
+
+const isPlaying = ref(false)
+const delay = ref(null)
+
+const start = () => {
+  isPlaying.value = true
+  delay.value = 2000
+}
+</script>
+
+<template>
+  <h1 class="ninja">Ninja Reaction Timer🥷</h1>
+  <button @click="start">
+    <span class="text">play</span>
+    <span class="perk">🌀</span>
+  </button>
+  <div class="cursor-container">
+    <Cursor1 />
+  </div>
+</template>
+
+<style scoped>
+.ninja {
+  /* height: 6em;
+  padding: 1.5em; */
+  will-change: filter;
+}
+.ninja:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+</style>
---
+++ src/components/cursor1.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <div class="cursor" ref="cursor"></div>
+  </div>
+</template>
+
+<script setup>
+import { ref, onMounted, onUnmounted } from "vue"
+
+// const x = ref(0)
+// const y = ref(0)
+const cursor = ref(null)
+
+function update(event) {
+  cursor.value.style.top = event.pageY + "px"
+  cursor.value.style.left = event.pageX + "px"
+}
+
+onMounted(() => window.addEventListener("mousemove", update))
+onUnmounted(() => window.removeEventListener("mousemove", update))
+</script>
+
+<style scoped>
+.cursor {
+  width: 20px;
+  height: 20px;
+  /* border: 0.5px solid white; */
+  border-radius: 50%;
+  position: absolute;
+  cursor: pointer;
+  transform: translate(-50%, -50%);
+  transition: border 300ms ease;
+  pointer-events: none;
+  background: url("../assets/shuriken.svg");
+  animation: spin 100ms linear infinite;
+}
+
+@keyframes spin {
+  0% {
+    transform: rotate(0deg);
+  }
+  100% {
+    transform: rotate(360deg);
+  }
+}
+</style>
