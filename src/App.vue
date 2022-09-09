c6643dfb49efdf456354b955468956e2e531b080
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-09-09 23:55:44 +0100

    :sparkles: add the result section

--- src/App.vue
+++ src/App.vue
@@ -5,15 +5,21 @@ import Block from "./components/Block.vue"
 
 const isPlaying = ref(false)
 const delay = ref(null)
+const score = ref(null)
 
 const start = () => {
   isPlaying.value = true
   delay.value = Math.floor(2000 + Math.random() * 5000)
 }
+
+const endGame = (reactionTime) => {
+  score.value = reactionTime
+  isPlaying.value = false
+}
 </script>
 
 <template>
-  <h1 class="ninja">Ninja Reaction 🥷</h1>
+  <h1 class="ninja">Ninja React🥷</h1>
   <button @click="start" :disabled="isPlaying">
     <span class="text">play</span>
     <span class="perk">🌀</span>
@@ -21,10 +27,18 @@ const start = () => {
   <div class="cursor-container">
     <Cursor1 />
   </div>
-  <Block v-if="isPlaying" :delay="delay" />
+  <Block v-if="isPlaying" :delay="delay" @end="endGame" />
+  <p>
+    Reaction time: <span class="score">{{ score }}</span
+    >ms
+  </p>
 </template>
 
-<style scoped>
+<style>
+#app {
+  max-width: 1024px;
+}
+
 .ninja {
   /* height: 6em;
   padding: 1.5em; */
@@ -36,5 +50,10 @@ const start = () => {
 
 .logo.vue:hover {
   filter: drop-shadow(0 0 2em #42b883aa);
+}
+
+.score {
+  font-family: "Assassin Ninja";
+  font-family: "Asian Ninja";
 }
 </style>
