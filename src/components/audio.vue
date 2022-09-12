d9b850151d56ac37b5aeb89556f13f3d8f1713dd
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-09-12 13:25:44 +0100

    add new icon and audio assets

---
+++ src/components/audio.vue
@@ -0,0 +1,13 @@
+<template>
+  <div class="audio-container">
+    <img src="../assets/volume-2.svg" alt="volume-button" />
+    <!--
+
+      Get a speaker icon and set that up to play Ninja sounds
+    -->
+  </div>
+</template>
+
+<script setup></script>
+
+<style></style>
