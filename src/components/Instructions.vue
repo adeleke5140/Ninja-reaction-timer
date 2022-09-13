5b61a2681ec77028ee90e8b65b4b2ecbc2cff1b4
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-09-13 11:27:24 +0100

    :sparkles: add instructions component

---
+++ src/components/Instructions.vue
@@ -0,0 +1,25 @@
+<template>
+  <div class="instructions">
+    <p>How fast are you as a Ninja?</p>
+    <p>
+      Your mission is to click on the box, as fast as you can once it appears!
+    </p>
+    <p>Are you ready to take in On, Shinobi?🥷🏼</p>
+  </div>
+</template>
+
+<script setup></script>
+
+<style>
+.instructions {
+  font-family: "Asian Ninja";
+  position: absolute;
+  top: 0;
+  left: 0;
+  text-align: left;
+  padding: 20px;
+  margin: 10px;
+  border: 1px dotted currentColor;
+  font-size: 0.8rem;
+}
+</style>
