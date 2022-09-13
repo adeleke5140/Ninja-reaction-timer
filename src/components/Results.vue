bdc8334839a204628ffc850705405eb695750811
Kehinde Adeleke <adelekekehinde06@gmail.com>
2022-09-13 15:46:41 +0100

    :sparkles: add the Results component
    add in code of the results component and also messages for the Ninja

---
+++ src/components/Results.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="result">
+    <p>
+      Reaction time: <span class="score">{{ score }}</span
+      >ms
+    </p>
+    <p v-if="score < 600">{{ ninjaResult }}, shinobi-kun</p>
+    <p v-else>You are not a Shinobu yet. {{ ninjaResult }}</p>
+  </div>
+</template>
+
+<script setup>
+import { onMounted, ref } from "vue"
+const props = defineProps(["score"])
+
+const ninjaResult = ref("")
+
+function getNinjaResult(score) {
+  console.log(score, "function fired")
+  if (score < 100) {
+    ninjaResult.value = "You are worthy of being  sensei!"
+  } else if (score > 100 && score <= 250) {
+    ninjaResult.value = "Ninja Fingers"
+  } else if (score > 250 && score <= 400) {
+    ninjaResult.value = "Fast Fingers"
+  } else if (score > 400 && score <= 600) {
+    ninjaResult.value = "Slow"
+  } else {
+    ninjaResult.value = "You need to go back to the training ground!"
+  }
+}
+
+onMounted(() => {
+  console.log("result component mounted")
+  getNinjaResult(props.score)
+})
+</script>
+
+<style>
+.result,
+.score {
+  font-family: "Assassin Ninja";
+  font-family: "Asian Ninja";
+}
+</style>
